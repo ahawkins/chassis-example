5504a3ff87bf6ccae7f55751c5440465324cc998
ahawkins <me@broadcastingadam.com>
2014-01-06 17:40:36 +0100

    Configure sidekiq redis

--- config/production.rb
+++ config/production.rb
@@ -3,3 +3,11 @@ twilio_auth_token = ENV.fetch 'TWILIO_AUTH_TOKEN'
 twilio_number = ENV.fetch 'TWILIO_NUMBER'
 
 SmsService.backend = SmsService::Twilio.new twilio_account_sid, twilio_auth_token, twilio_number
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV.fetch('REDISCLOUD_URL'), namespace: 'sidekiq' }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV.fetch('REDISCLOUD_URL'), namespace: 'sidekiq' }
+end
