87b8513ba0d3c56e93577c0c5498b36fc12587d3
ahawkins <me@broadcastingadam.com>
2014-01-08 07:48:38 +0100

    Models track their timestamps

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,4 +5,10 @@ class Group
 
   attr_accessor :name, :admin, :users
   attr_accessor :created_at, :updated_at
+
+  def save
+    self.created_at = Time.now.utc if new_record?
+    self.updated_at = Time.now.utc
+    super
+  end
 end
--- app/repos/adapters/in_memory_adapter.rb
+++ app/repos/adapters/in_memory_adapter.rb
@@ -1,15 +1,4 @@
 class InMemoryAdapter < Chassis::Repo::InMemoryAdapter
-  def create(record)
-    create_method = "create_#{record.class.name.underscore}"
-    send create_method, record if respond_to? create_method
-    super
-  end
-
-  def create_group(group)
-    group.created_at = Time.now.utc
-    group.updated_at = Time.now.utc
-  end
-
   def query_auth_token_with_code(klass, q)
     all(klass).find do |auth_token|
       auth_token.code == q.code
