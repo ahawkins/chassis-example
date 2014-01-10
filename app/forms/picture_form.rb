b677fea316affc9f3d209213ee144d2f03118767
ahawkins <me@broadcastingadam.com>
2014-01-11 00:19:51 +0100

    Backstage photo route for testing

--- app/forms/picture_form.rb
+++ app/forms/picture_form.rb
@@ -17,9 +17,23 @@ class PictureForm < Form
       end
     end
 
+    class ImageFileUpload
+      attr_reader :file
+
+      def initialize(file)
+        @file = file
+      end
+
+      def bytes
+        file.size
+      end
+    end
+
     def coerce(value)
       if value.is_a?(::Hash)
         MultipartImageUpload.new value
+      elsif value.is_a?(File)
+        ImageFileUpload.new value
       end
     end
   end
