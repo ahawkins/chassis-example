776e7685f563893ccfdeaf7d939b45e91f2fabd3
ahawkins <adam@hawkins.io>
2014-04-21 07:28:04 -0500

    Configure production environment

--- config/production.rb
+++ config/production.rb
@@ -5,8 +5,11 @@ twilio_account_sid = ENV.fetch 'TWILIO_ACCOUNT_SID'
 twilio_auth_token = ENV.fetch 'TWILIO_AUTH_TOKEN'
 twilio_number = ENV.fetch 'TWILIO_NUMBER'
 
-SmsService.backend = SmsService::TwilioBackend.new twilio_account_sid, twilio_auth_token, twilio_number
-ImageService.backend = ImageService::CloudinaryBackend.new ENV.fetch 'CLOUDINARY_URL'
+SmsService.register :twilio, SmsService::TwilioBackend.new twilio_account_sid, twilio_auth_token, twilio_number
+SmsService.use :twilio
+
+ImageService.register :cloudinary, ImageService::CloudinaryBackend.new ENV.fetch 'CLOUDINARY_URL'
+ImageService.use :cloudinary
 
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV.fetch('REDISCLOUD_URL'), namespace: 'sidekiq' }
@@ -19,6 +22,6 @@ end
 connection_pool = ConnectionPool::Wrapper.new(size: ENV.fetch('PUMA_MAX_THREADS').to_i) do
   Redis::Namespace.new 'repo', redis: Redis.new(url: ENV.fetch('REDISCLOUD_URL'))
 end
-Chassis::Repo.backend = RedisAdapter.new connection_pool
 
-Chassis::Repo.instance.initialize_storage!
+Chassis.repo.register :redis, RedisRepo.new(connection_pool)
+Chassis.repo.use :redis
